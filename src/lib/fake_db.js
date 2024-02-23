d8ebfcc96ad07ae5da2501c8f0528f3d9cd98d32
David Londono <davidlucky@gmail.com>
2024-02-22 22:16:59 -0500

    se sube ajuste

---
+++ src/lib/fake_db.js
@@ -0,0 +1,29 @@
+export const fakeData = [
+	{
+		id: 1,
+		name: "Ben Patton",
+		title: "First Post",
+		slug: "ben-patton",
+		date: "03-29-2023",
+		content: "This is the first post on the blog.",
+		published: true,
+	},
+	{
+		id: 2,
+		name: "Sirt Arthuer",
+		title: "First Post",
+		slug: "ben-patton",
+		date: "03-29-2023",
+		content: "This is the first post on the blog.",
+		published: true,
+	},
+	{
+		id: 3,
+		name: "Arturito",
+		title: "First Post",
+		slug: "ben-patton",
+		date: "03-29-2023",
+		content: "This is the first post on the blog.",
+		published: true,
+	},
+];
